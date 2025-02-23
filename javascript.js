62401e64ab49d87e2a6b80041ef991cda3830b62
BANKEBIHARI123 <banke201198@gmail.com>
2025-02-23 14:47:30 +0530

    portfolio-banke

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// This is a basic template. You can add JavaScript functionalities such as form validation, animations, etc.
+
+// Example: Simple form submission alert
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Your message has been sent!");
+});
